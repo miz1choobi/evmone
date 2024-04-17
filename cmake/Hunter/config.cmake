0cb95f18f6c32d32374076677f4036e5e2bb9987
miz1choobi <123825458+miz1choobi@users.noreply.github.com>
2024-04-17 14:51:56 +0330

    Update config.cmake

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,6 @@
 # evmone: Fast Ethereum Virtual Machine implementation
 # Copyright 2018 The evmone Authors.
 # SPDX-License-Identifier: Apache-2.0
-
 include(hunter_cmake_args)
 
 hunter_cmake_args(
